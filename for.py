57638bbbfc59f06729132eaa3d3534fe7a14106a
joseph70-1 <49540415+joseph70-1@users.noreply.github.com>
2025-01-13 05:54:04 -0700

    Delete for.py

--- for.py
+++
@@ -1,9 +0,0 @@
-fruits = ["Mangoes","Apples","Guavas","Grapes"]
-
-for x in fruits:
-    if x == "Apples":
-        print(x)
-        print("Apples are really your dream fruit!")
-
-for x in "Mangoes":
-    print(x)
